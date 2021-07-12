1952a9b1efdd681aa0177077e3f21c781da95d1f
KhoiLe <Khoilecontact@gmail.com>
2021-07-12 20:13:51 +0700

    first commit

--- SimpleWebBrowser/ViewController.swift
+++ SimpleWebBrowser/ViewController.swift
@@ -6,14 +6,107 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!
+    var progressView: UIProgressView!
+    var websites = ["apple.com", "facebook.com"]
+    
+    //direct the view to webView
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //add navigationBarItem
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
+        //a spacer
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        //refresh button
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        
+        //giving the default style for progress view
+        progressView = UIProgressView(progressViewStyle: .default)
+        //set layout size to fit content fully
+        progressView.sizeToFit()
+        //create a new BarButton using custom view
+        let progressButton = UIBarButtonItem(customView: progressView)
+        
+        let backwardButton = UIBarButtonItem(title: "Back", style: .plain, target: self, action:#selector(goBack))
+        let forwardButton = UIBarButtonItem(title: "Forward", style: .plain, target: self, action: #selector(goForward))
+        //what will be shown on toolbar
+        toolbarItems = [backwardButton, progressButton, spacer, refresh, forwardButton]
+        //show the tool bar
+        navigationController?.isToolbarHidden = false
+        
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+        
+        let url = URL(string: "https://" + websites[0])!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+    }
+    
+    @objc func openTapped() {
+        let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
+        for website in websites {
+            ac.addAction(UIAlertAction(title: website, style: .default, handler: openPage))
+        }
+        
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        
+        present(ac, animated: true)
+    }
+    
+    func openPage(action: UIAlertAction) {
+        guard let actionTitle = action.title else {return}
+        guard let url = URL(string: "http://" + actionTitle) else {return}
+        webView.load(URLRequest(url: url))
     }
 
-
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        progressView.progress = Float(webView.estimatedProgress)
+    }
+    
+    //factoring handle error
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.url
+        var showingAlert = true
+        
+        if let host = url?.host {
+            for website in websites {
+                if host.contains(website) {
+                    decisionHandler(.allow)
+                    showingAlert = false
+                    return
+                }
+            }
+        }
+        
+        if showingAlert == true {
+            let ac = UIAlertController(title: "Blocked", message: "This website has been blocked", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            present(ac, animated: true, completion: nil)
+        }
+        decisionHandler(.cancel)
+    }
+    
+    @objc func goBack() {
+        webView.goBack()
+    }
+    
+    @objc func goForward() {
+        webView.goForward()
+    }
 }
 
